634fa2446728a5c080cfe5fa4eee03e615016dc2
aleja <alejandraacaicedoo@gmail.com>
2025-05-29 22:06:15 -0500

    subiendo cambios

--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -5,3 +5,19 @@ router = APIRouter(prefix="/usuarios", tags=["usuarios"])
 @router.get("/")
 def listar_usuarios():
     return {"mensaje": "Lista de usuarios (simulada)"}
+
+@router.post("/")
+def crear_usuario(usuario: dict):
+    return {"mensaje": "Usuario creado", "usuario": usuario}
+
+@router.get("/{usuario_id}")
+def obtener_usuario(usuario_id: int):
+    return {"usuario_id": usuario_id, "mensaje": "Información del usuario"}
+
+@router.put("/{usuario_id}")
+def actualizar_usuario(usuario_id: int, usuario: dict):
+    return {"mensaje": f"Usuario {usuario_id} actualizado", "usuario": usuario}
+
+@router.delete("/{usuario_id}")
+def eliminar_usuario(usuario_id: int):
+    return {"mensaje": f"Usuario {usuario_id} eliminado"}
